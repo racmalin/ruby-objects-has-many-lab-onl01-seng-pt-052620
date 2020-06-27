16864c14dd34a898df2465471fdcf0fcf0500ccd
Kevin C Malin <racmalin@yahoo.com>
2020-06-27 02:44:56 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,29 +1,24 @@
 require 'pry'
 
   
-  class Artist
+class Artist
 
-  attr_accessor :name, :songs
-
-  @@song_count = 0
+  attr_accessor :name
 
   def initialize(name)
     @name = name
-    @songs = []
   end
 
   def add_song(song)
+    song.artist = self
+   end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
     @songs << song
     song.artist = self
     @@song_count += 1
-  # end
-
-  # def add_song_by_name(name)
-  #   song = Song.new(name)
-  #   @songs << song
-  #   song.artist = self
-  #   @@song_count += 1
-  # end
+  end
 
   # def self.song_count
   #   @@song_count
